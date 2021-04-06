96840a9eed9d6b621de00d809981f87dc0181f86
JoseLG03 <antares069@gmail.com>
2021-04-05 20:37:39 -0500

    function add to cart

--- js/app.js
+++ js/app.js
@@ -2,15 +2,44 @@
 
 const cart = document.querySelector("#divCart");
 
-const btnEmptyCart = document.querySelector("#emptyCart");
+const cartPrice = document.querySelector("#cartPrice");
 
-const listCart = document.querySelector("#listCart");
+const listCupcake= document.querySelector("#container");
+
+const tBody = document.querySelector("#listCart tbody");
+
+const btnEmptyCart = document.querySelector("#emptyCart");
 
 const btnAdd = document.querySelector(".cart_add");
 
 let shoppingCart = [];
 
-console.log(cart);
-console.log(btnEmptyCart);
-console.log(listCart);
-console.log(btnAdd);
+//console.log(cart);
+//console.log(cartPrice);
+//console.log(btnEmptyCart);
+//console.log(listCart);
+//console.log(tBody);
+
+//Load listeners
+
+    loadEventsListeners();
+
+    function loadEventsListeners(){
+ 
+        listCupcake.addEventListener("click", addToCart);
+
+        btnEmptyCart.addEventListener("click", emptyCart);
+    };
+
+    function addToCart(e){
+
+        e.preventDefault();
+
+        if(e.target.classList.contains('add')){
+            console.log("Agregar al carrito");
+        }
+    };
+
+    function emptyCart(){
+        console.log("Vaciar el carrito");
+    };

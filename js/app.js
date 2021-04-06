bcfd4e80221cc38b496cfef2b73055a7087590d3
JoseLG03 <antares069@gmail.com>
2021-04-05 21:10:40 -0500

    create object

--- js/app.js
+++ js/app.js
@@ -10,8 +10,6 @@ const tBody = document.querySelector("#listCart tbody");
 
 const btnEmptyCart = document.querySelector("#emptyCart");
 
-const btnAdd = document.querySelector(".cart_add");
-
 let shoppingCart = [];
 
 //console.log(cart);
@@ -36,10 +34,24 @@ let shoppingCart = [];
         e.preventDefault();
 
         if(e.target.classList.contains('add')){
-            console.log("Agregar al carrito");
+            const cupcakeSelected = e.target.parentElement.parentElement.parentElement;
+
+            readInfoCake(cupcakeSelected);
         }
     };
 
     function emptyCart(){
         console.log("Vaciar el carrito");
+    };
+
+    function readInfoCake(e){
+        console.log(e);
+        let infoCake ={
+            //img: e.querySelector('img').src,
+            //name: e.querySelector('h6 a').textContent,
+            //price: e.querySelector('product__item__price').textContent,
+            id: e.querySelector('a').getAttribute('data-id'),
+            count: 1
+        }
+        console.log(infoCake);
     };

1a46850130fc665c07d217bc0a20b4a5a11f38fd
JoseLG03 <antares069@gmail.com>
2021-04-05 19:11:27 -0500

    conts and let

---
+++ js/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const cart = document.querySelector("#divCart");
+
+const btnEmptyCart = document.querySelector("#emptyCart");
+
+const listCart = document.querySelector("#listCart");
+
+const btnAdd = document.querySelector(".cart_add");
+
+let shoppingCart = [];
+
+console.log(cart);
+console.log(btnEmptyCart);
+console.log(listCart);
+console.log(btnAdd);

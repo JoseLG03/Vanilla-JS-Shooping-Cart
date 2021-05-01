ca69e07e532fa17f9822451554356648ad34cad5
JoseLG03 <antares069@gmail.com>
2021-04-30 20:52:16 -0500

    Validate form

--- js/validateForm.js
+++ js/validateForm.js
@@ -1,3 +1,21 @@
 'use strict';
 
-console.log("Hello world");
+//variables
+
+const btnSend = document.querySelector("#send");
+
+//Event Listeners
+
+eventListeners();
+
+function eventListeners(){
+    document.addEventListener("DOMContentLoaded", startForm);
+}
+
+
+//functions
+
+function startForm(){
+    btnSend.disabled = true;
+    btnSend.classList.add("disabled");
+}

7bfa678eaaeebe96a813cb6741e8bc044639efec
JoseLG03 <antares069@gmail.com>
2021-05-01 20:42:53 -0500

    creating vars

--- js/validateForm.js
+++ js/validateForm.js
@@ -2,6 +2,10 @@
 
 //variables
 
+const inputName = document.querySelector("#inputName");
+const inputEmail = document.querySelector("#inputEmail");
+const inputMessage = document.querySelector("#inputMessage");
+
 const btnSend = document.querySelector("#send");
 
 //Event Listeners
@@ -10,12 +14,21 @@ eventListeners();
 
 function eventListeners(){
     document.addEventListener("DOMContentLoaded", startForm);
+    inputName.addEventListener("blur", validateForm);
 }
-
 
 //functions
 
 function startForm(){
     btnSend.disabled = true;
     //btnSend.classList.add("disabled");
+}
+
+function validateForm(e){
+    if(e.target.value.length > 0){
+        console.log("Si hay algo");
+    }else{
+        e.target.style.borderBottomColor = "red";
+        e.target.placeholder= "This field may not be empty";
+    }
 }
